291d76992e5936215ea9d5de8af23060ee5ecfd0
Maleehaakbar <maleehaashley@gmail.com>
2025-10-27 19:19:35 +0500

    add cmake input file

--- arr/arr.c
+++ arr/arr.c
@@ -3,5 +3,5 @@
 void array_size()
 {
     int arr[5] = {1,3,4,5,6};
-    printf("size of array is %ld", sizeof(arr));
+    printf("size of array is %ld\n", sizeof(arr));
 }
--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "sum.h"
+#include "arr.h"
+#include "template_header.h"
 int main()
 {   
     int s;
-   
     s = sum(3,5);
-    printf("sum is %d", s);
+    printf("sum is %d\n", s);
     array_size();
+    printf("project name is %s", project_name);
     return 0;
 }
