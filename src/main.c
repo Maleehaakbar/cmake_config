b4dc57748dc5781c670d60c722a0c952e1394c48
Maleehaakbar <maleehaashley@gmail.com>
2025-10-28 19:45:42 +0500

    add libzip via fetchcontent

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "arr.h"
 #include "template_header.h"
 #include "cJSON.h"
+#include "zipconf.h"
 int main()
 {   
     int s;
@@ -10,6 +11,7 @@ int main()
     printf("sum is %d\n", s);
     array_size();
     printf("cjson version %d\n",CJSON_VERSION_MAJOR);
-    printf("project name is %s", project_name);
+    printf("project name is %s\n", project_name);
+    printf("lipzip version is %s\n", LIBZIP_VERSION);
     return 0;
 }
